7675f73033892fdda2b08dc7c887eadb7d080d7b
Anto Mätas <anto.matas@nortal.com>
2023-07-04 18:29:09 +0300

    remove validations

--- src/main/java/ee/pocopay/homeassignment/domain/usecase/TransferFundsUseCase.java
+++ src/main/java/ee/pocopay/homeassignment/domain/usecase/TransferFundsUseCase.java
@@ -1,10 +1,6 @@
 package ee.pocopay.homeassignment.domain.usecase;
 
 import ee.pocopay.homeassignment.domain.ViolationType;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -12,17 +8,12 @@ import java.math.BigDecimal;
 
 public interface TransferFundsUseCase {
 
-    /*
-    TODO! For some reason this validation does not work.
-     Currently the validation has been added to the controller and TransferDto to ensure functionality, but it is not
-     ideal since the architectural pattern dictates use-case should not rely on web port.
-     */
-    void execute(@Valid Request request);
+    void execute(Request request);
 
-    record Request(@NotBlank String senderAccount,
-                   @NotNull BigDecimal amount,
-                   @NotBlank String recipientAccount,
-                   @NotBlank @Size(max = 300) String description) {
+    record Request(String senderAccount,
+                   BigDecimal amount,
+                   String recipientAccount,
+                   String description) {
     }
 
     @Getter
